4f5e74ec84ad7683360b28a244276d0cb8d105a1
sang <sang3141592@gmail.com>
2025-11-03 16:23:59 +0900

    feat(service): 로또 번호 일치 카운트 기능 구현

--- src/main/java/lotto/service/LottoNumberVerification.java
+++ src/main/java/lotto/service/LottoNumberVerification.java
@@ -1,4 +1,18 @@
 package lotto.service;
 
+import java.util.List;
+
+//로또 번호 일치 여부 검증
 public class LottoNumberVerification {
+    private List<Integer> Numbers;
+    private List<Integer> LottoNumbers;
+    public LottoNumberVerification(List<Integer> Numbers, List<Integer> LottoNumbers){
+        this.Numbers = Numbers;
+        this.LottoNumbers = LottoNumbers;
+    }
+    public int Verfication(){
+        int matchCount = (int)Numbers.stream().filter(num -> LottoNumbers.contains(num)).count();
+        return matchCount;
+    }
+
 }
