ce2e4945bebe5f650f4d98886b8d59494b90bf0e
sang <sang3141592@gmail.com>
2025-11-03 16:53:50 +0900

    feat(service): 보너스 번호 일치 여부 추가 구현

--- src/main/java/lotto/service/LottoNumberVerification.java
+++ src/main/java/lotto/service/LottoNumberVerification.java
@@ -1,18 +1,25 @@
 package lotto.service;
 
+import lotto.domain.BonusNum;
+import lotto.domain.Rank;
+
 import java.util.List;
 
 //로또 번호 일치 여부 검증
 public class LottoNumberVerification {
     private List<Integer> Numbers;
     private List<Integer> LottoNumbers;
-    public LottoNumberVerification(List<Integer> Numbers, List<Integer> LottoNumbers){
+    private int BonusNum;
+    public LottoNumberVerification(List<Integer> Numbers, List<Integer> LottoNumbers, int BonusNum){
         this.Numbers = Numbers;
         this.LottoNumbers = LottoNumbers;
+        this.BonusNum = BonusNum;
     }
-    public int Verfication(){
+    public int Counter(){
         int matchCount = (int)Numbers.stream().filter(num -> LottoNumbers.contains(num)).count();
         return matchCount;
     }
-
+    public boolean containBonusNum(){
+        return this.Numbers.contains(BonusNum);
+    }
 }
