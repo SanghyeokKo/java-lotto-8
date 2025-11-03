fa83165bb19b1d123d82d8541843becf2724756b
sang <sang3141592@gmail.com>
2025-11-03 19:59:29 +0900

    feat(service): 발급한 로또 랭크 카운트하는 기능 구현

--- src/main/java/lotto/service/RankCount.java
+++ src/main/java/lotto/service/RankCount.java
@@ -1,4 +1,24 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
 public class RankCount {
+    public Map<Rank, Integer> countRank(List<Lotto> myLotto, List<Integer> winNumbers, int bonusNum) {
+        Map<Rank, Integer> countRank = new EnumMap<>(Rank.class);
+        //발급한 모드 로또를 EnumMap에 저장(키:Rank,값:카운트값)
+        for (Lotto lotto : myLotto) {
+            LottoNumberVerification verification = new LottoNumberVerification(lotto.getNumbers(), winNumbers, bonusNum);
+            int matchCount = verification.Counter();
+            boolean cotainBonusNum = verification.containBonusNum();
+            CalculateRank calculateRank = new CalculateRank(matchCount, cotainBonusNum);
+            Rank rank = calculateRank.calculateRank();
+            countRank.put(rank, countRank.getOrDefault(rank,0)+1);
+        }
+        return countRank;
+    }
 }
